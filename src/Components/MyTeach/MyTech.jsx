5ae90362e1d1eca97cb8e6b05f7712e95815449a
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-27 17:32:19 +0600

    card section

--- src/Components/MyTeach/MyTech.jsx
+++ src/Components/MyTeach/MyTech.jsx
@@ -8,25 +8,33 @@ const MyTech = () => {
         <div className='container mx-auto'>
             <div className='text-center'>
                 <p>|| Skill I have</p>
-                <h2 className='text-3xl font-bold'> My Tech Stack</h2>
+                <h2 className='text-3xl font-bold inline-block border-b-4 border-rose-600 pb-1'> My Tech Stack</h2>
             </div>
-            
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-10">
+
+            <div className="relative grid grid-cols-1 md:grid-cols-2 gap-0.5 mt-10">
+
+                {/* divider */}
+                <div className="hidden md:block absolute left-1/2 top-0 bottom-0 w-0.5 bg-gray-400 mx-auto"></div>
+
+
                 {/* Languages */}
-                <div data-aos="zoom-in" className="shadow-2xl shadow-gray-400 bg-gray-300  py-5 space-y-5 rounded-3xl hover:shadow-4xl hover:shadow-red-800 hover:transition hover:duration-500 h-72 ">
-                   <div  className='mt-8 space-y-6'>
+                <div data-aos="zoom-in" className="shadow-2xl shadow-gray-400 bg-gray-300  py-5 space-y-5 rounded-3xl hover:shadow-4xl hover:shadow-red-800 transition-all duration-500 ease-in-out transform hover:scale-105 hover:-translate-y-2 hover:rotate-1 h-72 bg-gradient-to-br from-gray-200 to-gray-300">
+                    <div className='mt-8 space-y-6'>
                         <h2 className="text-center font-bold text-xl">Languages</h2>
-                        <div className="flex gap-4 items-center justify-center ">
+                        <div className="flex flex-col md:flex-row gap-4 items-center justify-center 
+">
                             <p className="gap-4 bg-gray-500 py-2 px-20 flex flex-col items-center rounded-2xl transition-transform duration-500 hover:scale-110"><img className="w-13" src="/public/js.png" alt="" /><span className="font-semibold">JavaScript</span></p>
                             <p className="gap-4 bg-gray-500 py-2 px-20 flex flex-col items-center rounded-2xl transition-transform duration-500 hover:scale-110"><img className="w-13 object-cover" src="/public/html.png" alt="" /><span className="font-semibold">HTML</span></p>
                             <p className="gap-4 bg-gray-500 py-2 px-20 flex flex-col items-center rounded-2xl transition-transform duration-500 hover:scale-110"><img className="w-13 h-13 object-cover" src="/public/css.png" alt="" /><span className="font-semibold">CSS</span></p>
                         </div>
-                   </div>
+                    </div>
                 </div>
+               
+               
                 {/* Fronted */}
-                <div data-aos="fade-up-left" className="shadow-2xl shadow-gray-400 bg-gray-300 gap-1 p-6 space-y-5 rounded-3xl hover:shadow-4xl hover:shadow-red-800 transition hover:duration-500 translate-y-28">
+                <div data-aos="fade-up-left" className="shadow-2xl shadow-gray-400 bg-gray-300 gap-1 p-6 space-y-5 rounded-3xl hover:shadow-4xl  transition hover:duration-500 md:mt-24  hover:shadow-red-800 transition-all duration-500 ease-in-out transform hover:scale-105 hover:-translate-y-2 hover:rotate-1">
                     <h2 className="text-center font-bold text-xl">Fronted</h2>
-                    <div className="gap-4  grid grid-cols-3  ">
+                    <div className="gap-4  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
                         <p className="gap-4 bg-gray-500   flex flex-col items-center justify-center rounded-2xl transition-transform duration-500 hover:scale-110"><img className="w-13 h-12 object-cover" src="/public/react.png" alt="" /><span className="font-semibold">React</span></p>
                         <p className="gap-4 bg-gray-500 justify-center px-20 flex flex-col items-center rounded-2xl transition-transform duration-500 hover:scale-110"><img className="w-13 h-12 object-cover" src="/public/tailwind.png" alt="" /><span className="font-semibold">Tailwind</span></p>
                         <p className="gap-4 bg-gray-500 justify-center px-20 flex flex-col items-center rounded-2xl transition-transform duration-500 hover:scale-110"><img className="w-13 h-13 object-cover mt-3" src="/public/farmer-mition.jpeg" alt="" /><span className="font-semibold">Framer Motion</span></p>
@@ -37,9 +45,9 @@ const MyTech = () => {
 
                 {/* tools */}
 
-                <div data-aos="fade-up-right" className="shadow-2xl shadow-gray-400 bg-gray-300  p-8 space-y-5 rounded-3xl hover:shadow-4xl hover:shadow-red-800 hover:transition hover:duration-500 ">
-                    <h2 className="text-center font-bold text-xl">Languages</h2>
-                    <div className="grid grid-cols-3 gap-4 items-center justify-center ">
+                <div data-aos="fade-up-right" className="shadow-2xl shadow-gray-400 bg-gray-300  p-8 space-y-5 rounded-3xl hover:shadow-4xl hover:shadow-red-800  bg-gradient-to-br from-gray-200 to-gray-300   transition duration-500 ease-in-out transform hover:scale-105 hover:-translate-y-2 hover:rotate-1" >
+                    <h2 className="text-center font-bold text-xl">Tools</h2>
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-center justify-center ">
                         <p className="gap-4 bg-gray-500 py-2 px-20 flex flex-col items-center rounded-2xl transition-transform duration-500 hover:scale-110"><img className="w-13" src="/public/github.png" alt="" /><span className="font-semibold">GitHub</span></p>
                         <p className="gap-4 bg-gray-500 py-2 px-20 flex flex-col items-center rounded-2xl transition-transform duration-500 hover:scale-110"><img className="w-13 object-cover" src="/public/netlify.png" alt="" /><span className="font-semibold">Netlify</span></p>
                         <p className="gap-4 bg-gray-500 py-2 px-20 flex flex-col items-center rounded-2xl transition-transform duration-500 hover:scale-110"><img className="w-13 h-13 object-cover" src="/public/figma.png" alt="" /><span className="font-semibold">Figma</span></p>
@@ -48,6 +56,7 @@ const MyTech = () => {
                 </div>
 
             </div>
+            
         </div>
     );
 };

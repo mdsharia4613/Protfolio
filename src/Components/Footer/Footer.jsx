9510a17b589aad8f213eb37bfbdd2cfee62e714d
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-29 20:51:52 +0600

    ccurrection

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { Link } from 'react-scroll';
+
 
 const Footer = () => {
     return (
@@ -22,19 +23,54 @@ const Footer = () => {
                         <a className="link link-hover">mdsharia4613@gmail.com</a>
                     </div>
                     <div>
-                        <h6 className="footer-title bg-gradient-to-r from-sky-400 to-cyan-300 bg-clip-text text-transparent">Sharia</h6>
-                        <a className="link link-hover">mdsharia4613@gmail.com</a>
+                        <h6 className="footer-title bg-gradient-to-r from-sky-400 to-cyan-300 bg-clip-text text-transparent">Call</h6>
+                        <a className="link link-hover">+8801993628220</a>
                     </div>
                 </nav>
                 <nav>
                     <h6 className="footer-title bg-gradient-to-r from-sky-400 to-cyan-300 bg-clip-text text-transparent">Quick Links</h6>
-                    <a className="link link-hover">Home</a>
-                    <a  className="link link-hover">Skills</a>
-                    <a className="link link-hover">Experience</a>
-                    <a className="link link-hover">Project</a>
+                    <Link to="home" smooth={true}
+                        duration={700}
+                        offset={-70} className=" transition underline-effect  cursor-pointer hover:drop-shadow-[0_0_10px_rgba(56,189,248,0.6)]">Home</Link>
+                    <Link
+                        to="about-section"
+                        smooth={true}
+                        duration={700}
+                        offset={-70}
+                        className="cursor-pointer  transition underline-effect bg-linear-to-l from-sky-400 to-cyan-300 bg-clip-text text-transparent hover:drop-shadow-[0_0_10px_rgba(56,189,248,0.6)]"
+                    >
+                        About
+                    </Link>
+                    <Link
+                        to="about-section"
+                        smooth={true}
+                        duration={700}
+                        offset={-70}
+                        className="cursor-pointer  transition underline-effect bg-linear-to-l from-sky-400 to-cyan-300 bg-clip-text text-transparent hover:drop-shadow-[0_0_10px_rgba(56,189,248,0.6)]"
+                    >
+                        Skills
+                    </Link>
+                    <Link
+                        to="about-section"
+                        smooth={true}
+                        duration={700}
+                        offset={-70}
+                        className="cursor-pointer  transition underline-effect bg-linear-to-l from-sky-400 to-cyan-300 bg-clip-text text-transparent hover:drop-shadow-[0_0_10px_rgba(56,189,248,0.6)]"
+                    >
+                        Education
+                    </Link>
+                    <Link
+                        to="about-section"
+                        smooth={true}
+                        duration={700}
+                        offset={-70}
+                        className="cursor-pointer  transition underline-effect bg-linear-to-l from-sky-400 to-cyan-300 bg-clip-text text-transparent hover:drop-shadow-[0_0_10px_rgba(56,189,248,0.6)]"
+                    >
+                        Project
+                    </Link>
                 </nav>
                 <nav>
-                    <h6 className="footer-title bg-gradient-to-r from-sky-400 to-cyan-300 bg-clip-text text-transparent">Social</h6>
+                    <h6 className="footer-title bg-linear-to-r from-sky-400 to-cyan-300 bg-clip-text text-transparent">Social</h6>
                     <a className="link link-hover">Facebook</a>
                     <a className="link link-hover">Linkdine</a>
                     <a className="link link-hover">Github</a>
